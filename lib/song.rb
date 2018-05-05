a5ecadf547b60977ccd49b5291e9d2d5135da3fb
Kayla Reopelle <kreopelle@gmail.com>
2018-05-05 11:03:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,6 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    if @@genres.include?(genre) == false
-      @@genres << genre
-    end
-
-    
-
   end
 
   def self.count
